c9cb0e1aeab31d10757380534304f05f17e39ad6
Bhat <ajay.bhat@sap.com>
2017-04-14 20:12:46 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+
+
+app.use((request, response, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${request.method} ${request.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+app.use((request,response,next) => {
+    response.render('maintenance.hbs', {
+        pageTitle: 'In maintenance',
+        message: 'We will be back soon'
+    })
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (request, response) => {
+
+    response.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Hello Ajay!'
+    });
+});
+
+app.get('/about', (request, response) => {
+    response.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+});
+
+app.get('/bad', (request, response) => {
+    response.send({
+        errorMessage: 'Error Handling the request'
+    });
+});
+
+//binds application to a port on machine
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
